e6ecb4a6c45397fab386191d6ce9ef779a0d2cbb
Vo Thanh Trung <66295737+UIT-SE-TrungVo@users.noreply.github.com>
2021-11-23 10:31:08 +0700

    #feat: visit history (ugly af)

--- lib/core/dashboard/widget/panel_header_widget.dart
+++ lib/core/dashboard/widget/panel_header_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:src/commons/l10n/generated/l10n.dart';
@@ -9,9 +10,15 @@ class DBPanelHeader extends StatelessWidget {
   String? _title;
   String? _sideText;
   Widget? _sideIcon;
+  late VoidCallback _onPressed;
 
   @override
-  DBPanelHeader({String? title, String? sideText, Widget? sideIcon, Key? key})
+  DBPanelHeader(
+      {String? title,
+      String? sideText,
+      Widget? sideIcon,
+      Key? key,
+      VoidCallback? onPressed})
       : super(key: key) {
     _title = title ?? "Tiêu đề trống";
     _sideText = sideText ?? S.current.dashboard_detail;
@@ -20,6 +27,7 @@ class DBPanelHeader extends StatelessWidget {
           'assets/svg/icon/details.svg',
           width: 16.w,
         );
+    _onPressed = onPressed ?? () {};
   }
 
   @override
@@ -46,6 +54,7 @@ class DBPanelHeader extends StatelessWidget {
                 SizedBox(width: 4.w),
                 _sideIcon!
               ]),
+              onTap: _onPressed,
             )
           ],
         )
